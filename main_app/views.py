b54ec8a818dbc730ec482f501672f38bc9ec9adf
Maksym <fand1l@outlook.com>
2025-09-27 18:52:15 +0300

    Fixed bugs and added register view

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib import messages
 from django.db.models import Q, F
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.core.paginator import Paginator
 from django.core.exceptions import ValidationError
 from django.db import transaction, IntegrityError
@@ -26,13 +28,14 @@ def items_list(request):
     ctx = {"items": items, "categories": categories, "q": q, "cat": cat}
     return render(request, "main_app/items_list.html", ctx)
 
+
 def items_detail(request, pk: int):
     item = get_object_or_404(Item.objects.select_related(), pk=pk, is_active=True)
 
     if request.method == "POST":
         if not request.user.is_authenticated:
             messages.error(request, "Увійдіть, щоб орендувати предмет.")
-            return redirect(reverse("items_detail", args=[item.pk]))
+            return redirect(reverse("main_app:items_detail", args=[item.pk]))
         instance = Rental(item=item, user=request.user)
         form = RentalForm(request.POST, instance=instance)
         if form.is_valid():
@@ -42,7 +45,7 @@ def items_detail(request, pk: int):
             rental.deposit_amount = item.deposit
             rental.save()
             messages.success(request, "Заявка створена! Очікує підтвердження.")
-            return redirect("my_rentals")
+            return redirect("main_app:my_rentals")
     else:
         form = RentalForm()
 
@@ -50,10 +53,12 @@ def items_detail(request, pk: int):
     ctx = {"item": item, "form": form, "last_rentals": last_rentals}
     return render(request, "main_app/items_detail.html", ctx)
 
+
 @login_required
 def my_rentals(request):
     rentals = Rental.objects.select_related("item").filter(user=request.user).order_by("-created_at")
     return render(request, "main_app/my_rentals.html", {"rentals": rentals})
+
 
 @login_required
 def rental_cancel(request, pk: int):
@@ -62,4 +67,19 @@ def rental_cancel(request, pk: int):
         rental.status = "cancelled"
         rental.save()
         messages.info(request, "Оренду скасовано.")
-    return redirect("rentals")
+    return redirect("main_app:my_rentals")
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Обліковий запис створено")
+            return redirect("main_app:items_list")
+        
+    else:
+        form = UserCreationForm()
+
+    return render(request, "main_app/auth/register.html", {"form":form})
