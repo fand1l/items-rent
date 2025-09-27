17b5e88f88c2514be67adffe5d3765c426e510ea
Maksym <fand1l@outlook.com>
2025-09-27 18:58:29 +0300

    Added server startup code into main.py and remove django_setup.py

--- django_setup.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ItemsRent.settings")
-django.setup()
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import django_setup
+# Server starter
+
+import os
+import sys
+import django
+from django.core.management import execute_from_command_line
+
+IP = "0.0.0.0"
+PORT = "8000"
+DEBUG = True
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ItemsRent.settings")
+    django.setup()
+    execute_from_command_line([sys.argv[0], "runserver", f"{IP}:{PORT}"])
