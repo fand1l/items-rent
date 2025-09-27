d8e8edf22263c02b32b26ff023af4ec70f26486e
Maksym <fand1l@outlook.com>
2025-09-27 18:51:51 +0300

    Added 3 urls for auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = "main_app"
+
 urlpatterns = [
     path("", views.items_list, name="items_list"),
     path("items/<int:pk>/", views.items_detail, name="items_detail"),
     path("rentals/", views.my_rentals, name="my_rentals"),
     path("rentals/<int:pk>/cancel/", views.rental_cancel, name="rental_cancel"),
+
+    path("account/register", views.register, name="register"),
+    path("account/login", auth_views.LoginView.as_view(template_name="main_app/auth/login.html"), name="login"),
+    path("account/logout", auth_views.LogoutView.as_view(), name="logout")
 ]
